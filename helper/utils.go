0f7c908524a25bb6d12e1a8bfc2631a5109d6212
keelii <keeliizhou@gmail.com>
2026-02-15 23:52:10 +0800

    sv

--- helper/utils.go
+++ helper/utils.go
@@ -11,14 +11,31 @@ import (
 	"golang.org/x/net/html"
 )
 
-func FlattenTOC(input string) string {
-	var err error
-	var doc *html.Node
-	doc, err = html.Parse(strings.NewReader(input))
+// FlattenTOC 扁平化目录结构
+// listType: "ul" 或 "ol"，指定要查找的列表类型
+func FlattenTOC(input string, listType string) string {
+	doc, err := html.Parse(strings.NewReader(input))
 	if err != nil {
 		log.Println("解析 HTML 失败:", err)
 		return input
 	}
+
+	// 查找并修复指定类型的 toc-list
+	var fixTocList func(*html.Node)
+	fixTocList = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == listType {
+			for _, attr := range n.Attr {
+				if attr.Key == "id" && attr.Val == "toc-list" {
+					flattenTocList(n, listType)
+					return
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			fixTocList(c)
+		}
+	}
+
 	fixTocList(doc)
 
 	var buf strings.Builder
@@ -29,40 +46,24 @@ func FlattenTOC(input string) string {
 	return buf.String()
 }
 
-func fixTocList(n *html.Node) {
-	if n.Type == html.ElementNode && n.Data == "ol" {
-		for _, attr := range n.Attr {
-			if attr.Key == "id" && attr.Val == "toc-list" {
-				flattenTocList(n)
-				return
-			}
-		}
-	}
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		fixTocList(c)
-	}
-}
+// flattenTocList 扁平化指定类型的列表
+func flattenTocList(list *html.Node, listType string) {
+	var items []*html.Node
 
-// flattenTocList 将 <ul id="toc-list"><li><ul>...items...</ul></li></ul>
-// 转换为 <ul id="toc-list">...items...</ul>
-func flattenTocList(ul *html.Node) {
-	var items []*html.Node // 存储真正的列表项
-
-	// 遍历直接子节点
-	for child := ul.FirstChild; child != nil; {
+	for child := list.FirstChild; child != nil; {
 		next := child.NextSibling
 
 		if child.Type == html.ElementNode && child.Data == "li" {
-			// 查找这个 li 内部的 ul
+			// 查找 li 内部指定类型的列表
 			for grandchild := child.FirstChild; grandchild != nil; {
 				gcNext := grandchild.NextSibling
 
-				if grandchild.Type == html.ElementNode && grandchild.Data == "ul" {
-					// 提取这个内部 ul 的所有 li 子项
+				// 只检查指定的列表类型
+				if grandchild.Type == html.ElementNode && grandchild.Data == listType {
+					// 提取内部列表的所有 li 项
 					for item := grandchild.FirstChild; item != nil; {
 						itemNext := item.NextSibling
 						if item.Type == html.ElementNode && item.Data == "li" {
-							// 从原位置移除
 							grandchild.RemoveChild(item)
 							items = append(items, item)
 						}
@@ -71,16 +72,14 @@ func flattenTocList(ul *html.Node) {
 				}
 				grandchild = gcNext
 			}
-			// 移除这个包装用的 li
-			ul.RemoveChild(child)
+			list.RemoveChild(child)
 		}
 
 		child = next
 	}
 
-	// 将提取的 items 添加到 ul 中
 	for _, item := range items {
-		ul.AppendChild(item)
+		list.AppendChild(item)
 	}
 }
 
