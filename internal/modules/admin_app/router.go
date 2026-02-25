1200ec255c1a0f22275c31c3fd71e0fbd4e9bd5d
keelii <keeliizhou@gmail.com>
2026-02-25 15:02:40 +0800

    sv

--- internal/modules/admin_app/router.go
+++ internal/modules/admin_app/router.go
@@ -28,4 +28,24 @@ func RegisterRouter(
 	adminGroup.Get("/posts_list", func(c fiber.Ctx) error {
 		return RenderAdminView(c, "posts_list", fiber.Map{}, "")
 	}).Name("admin_app.posts_list")
+
+	uiGroup := adminGroup.Group("/ui")
+	uiGroup.Get("/buttons", func(c fiber.Ctx) error {
+		return RenderAdminView(c, "admin_app/ui/buttons", fiber.Map{}, "")
+	}).Name("admin_app.ui.buttons")
+	uiGroup.Get("/icons", func(c fiber.Ctx) error {
+		return RenderAdminView(c, "admin_app/ui/icons", fiber.Map{}, "")
+	}).Name("admin_app.ui.icons")
+	uiGroup.Get("/forms", func(c fiber.Ctx) error {
+		return RenderAdminView(c, "admin_app/ui/forms", fiber.Map{}, "")
+	}).Name("admin_app.ui.forms")
+	uiGroup.Get("/navigation", func(c fiber.Ctx) error {
+		return RenderAdminView(c, "admin_app/ui/navigation", fiber.Map{}, "")
+	}).Name("admin_app.ui.navigation")
+	uiGroup.Get("/feedback", func(c fiber.Ctx) error {
+		return RenderAdminView(c, "admin_app/ui/feedback", fiber.Map{}, "")
+	}).Name("admin_app.ui.feedback")
+	uiGroup.Get("/data", func(c fiber.Ctx) error {
+		return RenderAdminView(c, "admin_app/ui/data", fiber.Map{}, "")
+	}).Name("admin_app.ui.data")
 }
